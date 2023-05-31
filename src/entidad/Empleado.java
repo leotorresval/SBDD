053381e89cbb65909309973d649aba9d90e2a697
leotorresval <leotorresval92@gmail.com>
2023-05-31 12:12:16 -0500

    edad a;adida

--- src/entidad/Empleado.java
+++ src/entidad/Empleado.java
@@ -10,11 +10,13 @@ package entidad;
  */
 public class Empleado {
     private String cedula, nombre, apellido;
+    private int edad;
 
-    public Empleado(String cedula, String nombre, String apellido) {
+    public Empleado(String cedula, String nombre, String apellido,int edad) {
         this.cedula = cedula;
         this.nombre = nombre;
         this.apellido = apellido;
+	this.edad=edad;
     }
 
     /**
